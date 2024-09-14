0b866cc5a119f4eae202efc638d1b658aac1edb8
9raoka <yusakukuraoka@hiroshima-u.ac.jp>
2024-09-14 09:55:41 +0000

    git commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# GiikuHac14-BE
+# GiikuHac14-BE　環境構築方法
+
+## コンテナ作成
+以下のコマンドを実行
+```
+$ docker-compose build
+
+$ docker-compose run --entrypoint "poetry install --no-root" demo-app
+
+$ docker-compose up
+``` 
+
+## データベース作成
+apiコンテナにアタッチし、ターミナルで以下を実行
+```
+$ poetry run alembic upgrade head
+```
+データベースのバージョンが更新されるたびにこのコマンドを実行する必要があり
--- api/main.py
+++ api/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.routers import stall, user, review, item, auth
 
 app = FastAPI()
+
+# CORSの設定を追加
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://10.31.1.65:3000"],  # フロントエンドのURL（ローカルホストの場合）
+    allow_credentials=True,
+    allow_methods=["*"],  # すべてのHTTPメソッドを許可
+    allow_headers=["*"],  # すべてのHTTPヘッダーを許可
+)
+
 app.include_router(stall.router)
 app.include_router(user.router)
 app.include_router(review.router)
